bba207265eecbfac47b558be804fe8aa1a9ea401
Bianca Dellevedove Matsumura <bdmatsumura@sga.pucminas.br>
2024-09-27 07:54:12 -0300

    Login Teste + SQLite

--- LoginWindow.xaml.cs
+++ LoginWindow.xaml.cs
@@ -1,37 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Mader_Control_System.Login
 {
-    /// <summary>
-    /// Lógica interna para LoginWindow.xaml
-    /// </summary>
     public partial class LoginWindow : Window
     {
+       
+
         public LoginWindow()
         {
             InitializeComponent();
         }
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+          
+            string username = UsernameTextBox.Text;
 
+            
+            if (username == "admin")
+            {
+               
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Nome de usuário incorreto!", "Erro", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
