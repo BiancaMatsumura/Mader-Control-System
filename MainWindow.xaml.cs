546489a7fc4d35cbd1a63b43c6d97f4702965fa8
Bianca Dellevedove Matsumura <bdmatsumura@sga.pucminas.br>
2024-09-28 11:57:52 -0300

    telas e animações

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,5 +8,31 @@ namespace Mader_Control_System
         {
             InitializeComponent();
         }
+
+        private void Close_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+
+            this.Close();
+        }
+
+        private void Minimized_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void FileAdd_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (InfoGrid.Visibility == Visibility.Collapsed)
+            {
+                InfoGrid.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                InfoGrid.Visibility = Visibility.Collapsed;
+            }
+        }
     }
+
+
 }
